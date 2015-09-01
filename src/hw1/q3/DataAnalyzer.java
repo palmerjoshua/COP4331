69aae9a39564faf3d30716e24740948bc7987ace
= <palmerjoshua2013@fau.edu>
2015-09-01 13:22:26 -0400

    added file io and javadocs

--- src/hw1/q3/DataAnalyzer.java
+++ src/hw1/q3/DataAnalyzer.java
@@ -4,18 +4,38 @@ import java.util.LinkedList;
 
 public class DataAnalyzer {
 
+    /**
+     * Constructor that initializes dataList with an existing LinkedList.
+     * @param in The LinkedList whose data will populate the DataAnalyzer.
+     * @see #newSequence(LinkedList)
+     *
+     */
     public DataAnalyzer(LinkedList<Integer> in) {
         newSequence(in);
     }
 
+    /**
+     * Default constructor that initializes dataList as an empty list.
+     * @see #dataList
+     */
     public DataAnalyzer() {
         dataList = new LinkedList<Integer>();
     }
 
+    /**
+     * Re-initializes dataList with a new LinkedList
+     * @param newList The LinkedList whose data will populate the DataAnalyzer.
+     * @see #dataList
+     */
     public void newSequence(LinkedList<Integer> newList) {
         dataList = new LinkedList<Integer>(newList);
     }
 
+    /**
+     * Creates a String containing the sequence of integers stored in dataList
+     * @return String of integers separated by commas
+     * @see #dataList
+     */
     public String getSequence() {
         int length = dataList.size();
         if (length < 1) {return "";}
@@ -27,6 +47,11 @@ public class DataAnalyzer {
         return String.join(", ", sequence_holder);
     }
 
+    /**
+     * Computes the average value in the dataList
+     * @return The average value
+     * @see #dataList
+     */
     public double average() {
         int length = dataList.size();
         if (length < 1){return 0.0;}
@@ -35,23 +60,44 @@ public class DataAnalyzer {
             sum += i;
         }
         return (double)sum / (double)length;
-    }
+    }// todo test what happens when dataList is empty for all these functions
 
+    /**
+     * Computes the minimum value in the dataList.
+     * @return The smallest value stored in the dataList.
+     * @see #max()
+     * @see #sortedDataList()
+     */
     public int min() {
         LinkedList<Integer> sorted = sortedDataList();
         return sorted.getFirst();
     }
 
+    /**
+     * Computes the maximum value in the dataList.
+     * @return The largest value stored in the dataList.
+     * @see #min()
+     * @see #sortedDataList()
+     */
     public int max() {
         LinkedList<Integer> sorted = sortedDataList();
         return sorted.getLast();
     }
 
+    /**
+     * Creates a new list containing the values in dataList sorted in ascending order.
+     * @return A new, sorted dataList.
+     * @see #min()
+     * @see #max()
+     */
     private LinkedList<Integer> sortedDataList() {
         LinkedList<Integer> sorted = new LinkedList<Integer>(dataList);
         sorted.sort(null);
         return sorted;
     }
 
+    /**
+     * The LinkedList that stores the data to be analyzed.
+     */
     private LinkedList<Integer> dataList;
 }
--- src/hw1/q3/DataAnalyzerTester.java
+++ src/hw1/q3/DataAnalyzerTester.java
@@ -1,5 +1,7 @@
 package hw1.q3;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -7,13 +9,29 @@ import java.util.regex.Pattern;
 
 
 public class DataAnalyzerTester {
+    /**
+     * Regular expression pattern that matches sequences of integers
+     * separated by spaces, or the letter 'q'.
+     * @see #validInput(String)
+     */
     private static final Pattern inputPattern = Pattern.compile("^q$|^(\\d+ *)+$");
 
+    /**
+     * Tests a string against a regular expression.
+     * @param userInput String to be tested.
+     * @return True if the String matches, False if it does not match.
+     * @see #inputPattern
+     */
     private static boolean validInput(String userInput) {
         Matcher input_matcher = inputPattern.matcher(userInput);
         return input_matcher.matches();
     }
 
+    /**
+     * Converts a string of integers into a LinkedList of integers.
+     * @param userString An already-validated string of integers.
+     * @return A LinkedList of integers.
+     */
     private static LinkedList<Integer> stringToList(String userString) {
         LinkedList<Integer> result = new LinkedList<Integer>();
         String[] splitString = userString.split(" ");
@@ -25,20 +43,27 @@ public class DataAnalyzerTester {
         return result;
     }
 
+    /**
+     * Helper function to create a string for printing to the console and saving to a file.
+     * @param da A DataAnalyzer whose dataList has already been populated.
+     * @return A string containing the DataAnalyzer's data.
+     */
     private static String makeOutputString(DataAnalyzer da) {
         String result = "Sequence: " + da.getSequence() + "\n";
         result += "Min: " + da.min() + "\n";
         result += "Max: " + da.max() + "\n";
         result += "Average: " + da.average();
         return result;
-    }
+    } // todo make header for output file
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LinkedList<Integer> inputList;
         DataAnalyzer da = new DataAnalyzer();
         Scanner in = new Scanner(System.in);
-        String userInput = "";
+        PrintWriter pw;
+        String output;
 
+        String userInput = "";
         while (!userInput.equals("q")) {
             System.out.println("(Press 'q' to quit)");
             System.out.print("Enter a sequence of numbers separated by spaces: ");
@@ -48,8 +73,11 @@ public class DataAnalyzerTester {
                 if (!userInput.equals("q")) {
                     inputList = stringToList(userInput);
                     da.newSequence(inputList);
-                    // todo write to file
-                    System.out.println(makeOutputString(da));
+                    output = makeOutputString(da);
+                    pw = new PrintWriter("q3_output.txt");
+                    System.out.println(output);
+                    pw.println(output);
+                    pw.close();
                 }
             } else {
                 System.out.println("INVALID INPUT");
