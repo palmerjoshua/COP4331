a7c04bb96bb43cfea856b4845153c7e08ff156fd
= <palmerjoshua2013@fau.edu>
2015-10-06 13:37:03 -0400

    added comparator functions

--- src/hw3/ch4/q1/Student.java
+++ src/hw3/ch4/q1/Student.java
@@ -1,7 +1,6 @@
 package hw3.ch4.q1;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 
 public class Student { // todo implement comparators
@@ -19,9 +18,54 @@ public class Student { // todo implement comparators
         return new Date(enrollmentDate.getTime());
     }
 
-    public static void main(String[] args) {
-        ArrayList<Student> students = new ArrayList<>();
-        // todo sort by name, print output
-        // todo sort by date, print output
+    public static Comparator<Student> getCompByName() {
+        return new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+                return s1.getName().compareTo(s2.getName());
+            }
+        };
     }
+
+    public static Comparator<Student> getCompByDate() {
+        return new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+                return s1.getEnrollmentDate().compareTo(s2.getEnrollmentDate());
+            }
+        };
+    }
+
+    private static ArrayList<Student> getExampleStudents() {
+        ArrayList<Student> students = new ArrayList<>();
+        Calendar c1 = Calendar.getInstance(),
+                c2 = Calendar.getInstance(),
+                c3 = Calendar.getInstance();
+        c1.set(Calendar.MONTH, Calendar.JANUARY, Calendar.DATE, 1, Calendar.YEAR, 1970);
+        c2.set(Calendar.MONTH, Calendar.JUNE, Calendar.DATE, 9, Calendar.YEAR, 1990);
+        c3.set(Calendar.MONTH, Calendar.APRIL, Calendar.DATE, 4, Calendar.YEAR, 1994);
+
+        Student s1 = new Student("John S", c1.getTime()),
+                s2 = new Student("Joshua P", c2.getTime()),
+                s3 = new Student("Natasha B", c3.getTime());
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        return students;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Student> students = getExampleStudents();
+        Collections.sort(students, Student.getCompByName());
+        for (Student s: students) {
+            System.out.println(s.getName());
+            System.out.println(s.getEnrollmentDate().toString());
+        }
+        System.out.println();
+        Collections.sort(students, Student.getCompByDate());
+        for (Student s: students) {
+            System.out.println(s.getName());
+            System.out.println(s.getEnrollmentDate().toString());
+        }
+    } // todo check why date defaults to 05 for all students
 }
--- src/hw3/ch4/q1/StudentTest.java
+++ src/hw3/ch4/q1/StudentTest.java
@@ -1,5 +1,7 @@
 package hw3.ch4.q1;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import junit.framework.TestCase;
 
@@ -28,5 +30,64 @@ public class StudentTest extends TestCase {
         Date d1 = s1.getEnrollmentDate(),
              d2 = cal.getTime();
         assertEquals(d1.getTime(), d2.getTime());
+    }
+
+    public void testGetCompByName() throws Exception {
+        Student s2 = new Student("Bob Boberson", cal.getTime()),
+                s3 = new Student("Harold Harrison", cal.getTime());
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s3);
+        students.add(s2);
+        Collections.sort(students, Student.getCompByName());
+        assertTrue(isSortedByName(students));
+    }
+
+    private static boolean isSortedByName(ArrayList<Student> al) {
+        boolean sorted = true;
+        for (int i=1; i < al.size(); i++)  {
+            Student s1 = al.get(i-1),
+                    s2 = al.get(i);
+            if (s1.getName().compareTo(s2.getName()) > 0)
+                sorted = false;
+        }
+        return sorted;
+    }
+
+    private static boolean isSortedByDate(ArrayList<Student> al) {
+        boolean sorted = true;
+        for (int i=1; i < al.size(); i++)  {
+            Student s1 = al.get(i-1),
+                    s2 = al.get(i);
+            if (s1.getEnrollmentDate().compareTo(s2.getEnrollmentDate()) > 0)
+                sorted = false;
+        }
+        return sorted;
+    }
+
+    public void testGetCompByDate() throws Exception {
+        Calendar c2 = Calendar.getInstance(),
+                 c3 = Calendar.getInstance();
+        c2.set(Calendar.MONTH, Calendar.JANUARY);
+        c2.set(Calendar.DATE, 1);
+        c2.set(Calendar.YEAR, 2013);
+
+        c3.set(Calendar.MONTH, Calendar.JUNE);
+        c3.set(Calendar.DATE, 1);
+        c3.set(Calendar.YEAR, 2013);
+
+        Student s2 = new Student("Bob", c2.getTime()),
+                s3 = new Student("Harry", c3.getTime());
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        Collections.sort(students, Student.getCompByDate());
+        assertTrue(isSortedByDate(students));
+
+
+
+
+
     }
 }
