f70bf1fa47e1e7782283ff9174d5fece63578531
palmerjoshua <palmerjoshua2013@fau.edu>
2015-10-09 17:56:24 -0400

    modified textbook code to display 4 cars

---
+++ src/hw3/ch4/q3/AnimationTester.java
@@ -0,0 +1,51 @@
+package hw3.ch4.q3;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ This program implements an animation that moves
+ a car shape.
+ */
+public class AnimationTester
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+
+        final MoveableShape[] shapes = {new CarShape(0, 0, CAR_WIDTH), new CarShape(0, 101, CAR_WIDTH),
+                                        new CarShape(0, 201, CAR_WIDTH), new CarShape(0, 301, CAR_WIDTH)
+        };
+
+        ShapeIcon icon = new ShapeIcon(shapes,
+                ICON_WIDTH, ICON_HEIGHT);
+
+        final JLabel label = new JLabel(icon);
+        frame.setLayout(new FlowLayout());
+        frame.add(label);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        final int DELAY = 100;
+        // Milliseconds between timer ticks
+        Timer t = new Timer(DELAY, new
+                ActionListener()
+                {
+                    public void actionPerformed(ActionEvent event)
+                    {
+                        for (MoveableShape shape : shapes) {
+                            shape.translate(1, 0);
+                        }
+                        label.repaint();
+                    }
+                });
+        t.start();
+    }
+
+    private static final int ICON_WIDTH = 400;
+    private static final int ICON_HEIGHT = 408;
+    private static final int CAR_WIDTH = 100;
+}
---
+++ src/hw3/ch4/q3/ShapeIcon.java
@@ -0,0 +1,42 @@
+package hw3.ch4.q3;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ An icon that contains a moveable shape.
+ */
+public class ShapeIcon implements Icon
+{
+    public ShapeIcon(MoveableShape[] shapes,
+                     int width, int height)
+    {
+        this.shapes = shapes;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getIconWidth()
+    {
+        return width;
+    }
+
+    public int getIconHeight()
+    {
+        return height;
+    }
+
+    public void paintIcon(Component c, Graphics g, int x, int y)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+        for(MoveableShape shape : shapes) {
+            shape.draw(g2);
+        }
+    }
+
+    private int width;
+    private int height;
+    private MoveableShape[] shapes;
+}
+
+
+
