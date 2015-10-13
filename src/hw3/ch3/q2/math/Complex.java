577141d84b1ddd164618fec5785c78693a152a59
palmerjoshua <palmerjoshua2013@fau.edu>
2015-10-12 22:05:30 -0400

    display all method results in main; change example and test values to conform to requirements; added javadocs

--- src/hw3/ch3/q2/math/Complex.java
+++ src/hw3/ch3/q2/math/Complex.java
@@ -1,39 +1,71 @@
 package hw3.ch3.q2.math;
-
-
 import java.text.DecimalFormat;
 
+/**
+ * Represents a complex number with two doubles:
+ * one to represent the real part, and the other
+ * to represent the imaginary part (the coefficient of i).
+ */
 public class Complex implements Mathable {
     private double real, imaginary;
-    private final DecimalFormat format = new DecimalFormat("0.#####");
+    private final DecimalFormat format = new DecimalFormat("0.###");
 
     public Complex(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
+
     public Complex(double real) {
         this.real = real;
         this.imaginary = 0.0;
     }
 
+    /**
+     * Computes the complex conjugate of the instantiated Complex.
+     * @return a Complex with the same real part and a negative
+     * imaginary part.
+     */
     public Complex conj() {
         return new Complex(real, -imaginary);
     }
 
+    /**
+     * Computes the sum of two complex numbers.
+     * @param c2 The second Complex in the equation.
+     * @return a Complex representing the sum of two
+     * Complexes.
+     */
     public Complex add(Complex c2) {
         return new Complex(this.real+c2.real, this.imaginary+c2.imaginary);
     }
 
+    /**
+     * Computes the difference between two complex numbers.
+     * @param c2 a Complex representing the subtrahend.
+     * @return a Complex representing the difference.
+     */
     public Complex sub(Complex c2) {
         return new Complex(this.real-c2.real, this.imaginary-c2.imaginary);
     }
 
+    /**
+     * Computes the product of two complex numbers.
+     * @param c2 a Complex representing the multiplier.
+     * @return a Complex representing the product.
+     */
     public Complex mult(Complex c2) {
         double newReal = (real * c2.r()) - (imaginary * c2.i()),
                newImag = (real * c2.i()) + (imaginary * c2.r());
         return new Complex(newReal, newImag);
     }
 
+    /**
+     * Computes the quotient of two complex numbers.
+     * @param c2 a Complex representing the divisor.
+     * @return a Complex representing the quotient.
+     * @throws IllegalArgumentException if the divisor
+     * is zero.
+     */
     public Complex div(Complex c2) {
         if (c2.r() == 0.0 && c2.i() == 0.0) {
             throw new IllegalArgumentException("Attempted to divide by zero.");
@@ -46,22 +78,49 @@ public class Complex implements Mathable {
         return new Complex(newReal, newImag);
     }
 
+    /**
+     * Computes the real part of a complex number.
+     * @return a double representing the real part.
+     */
     public double r() {
         return real;
     }
 
+    /**
+     * Computes the imaginary part of a complex number.
+     * @return a double representing the coefficient of i.
+     */
     public  double i() {
         return imaginary;
     }
 
+    /**
+     * Helper function that converts the real part of
+     * a complex number into a String
+     * @return a String containing the real number if
+     * it is not zero, or an empty String if the real
+     * number is zero.
+     */
     private String realString() {
         return (real!=0) ? format.format(real) : "";
     }
 
+    /**
+     * Helper function that returns a '+', '-', or an
+     * empty String based on the contents of this.real
+     * and this.imaginary.
+     * @return a String containing an operator, or an empty String.
+     */
     private String sepString() {
         return (imaginary != 0 && real != 0) ? ((imaginary > 0) ? " + " : " - ") : "";
     }
 
+    /**
+     * Helper function that formats the imaginary part
+     * of a complex number into a readable String.
+     * @return a String representing the imaginary
+     * part of a complex number.
+     */
     private String imaginaryString() {
         if (imaginary == 0.0) {
             return "";
@@ -72,20 +131,44 @@ public class Complex implements Mathable {
         }
     }
 
+    /**
+     * Converts a Complex into a human-readable String.
+     * @return String representing this.
+     */
     public String toString() {
         return realString() + sepString() + imaginaryString();
     }
 
+    /**
+     * Determines whether two Complexes are equivalent.
+     * @param c2 The second Complex to compare to this.
+     * @return True if they are equal, False if they are
+     * unequal.
+     */
     public boolean equals(Complex c2) {
         return real == c2.real && imaginary == c2.imaginary;
     }
 
-    public static void main(String[] args) { // TODO display more functions
-        Complex c1 = new Complex(2, 4),
-                c2 = new Complex(-1, 5.3),
-                c3 = new Complex(0.0, -5.1);
-        System.out.println(c1.toString());
-        System.out.println(c2.toString());
-        System.out.println(c3.toString());
+    public static void main(String[] args) {
+        Complex c1 = new Complex(3, 2),
+                c2 = new Complex(3, -2);
+
+        Complex sum        = c1.add(c2),
+                difference = c1.sub(c2),
+                product    = c1.mult(c2),
+                quotient   = c1.div(c2),
+                conjugate  = c1.conj();
+
+        String[] strings = {
+                String.format("(%s) + (%s) = %s", c1.toString(), c2.toString(), sum.toString()),
+                String.format("(%s) - (%s) = %s", c1.toString(), c2.toString(), difference.toString()),
+                String.format("(%s) * (%s) = %s", c1.toString(), c2.toString(), product.toString()),
+                String.format("(%s) / (%s) = %s", c1.toString(), c2.toString(), quotient.toString()),
+                String.format("Conjugate of (%s) is (%s)", c1.toString(), conjugate.toString())
+        };
+
+        for (String s : strings) {
+            System.out.println(s);
+        }
     }
 }
--- src/hw3/ch3/q2/math/ComplexTest.java
+++ src/hw3/ch3/q2/math/ComplexTest.java
@@ -3,26 +3,12 @@ import junit.framework.TestCase;
 
 public class ComplexTest extends TestCase {
 
-    private Complex c, c2, c3;
+    private Complex c1, c2;
 
     public void setUp() throws Exception {
         super.setUp();
-        c = new Complex(20, 4);
-        c2 = new Complex(0, -5);
-        c3 = new Complex(-4, 0.34);
-    }
-
-    public void testAdd() {
-        Complex second = new Complex(-3, -6),
-                result = c.add(second);
-        String resultString = result.toString();
-        assertEquals("17 - 2i", resultString);
-    }
-
-    public void testSub() {
-        Complex second = new Complex(1, -2),
-                result = c.sub(second);
-        assertEquals("19 + 6i", result.toString());
+        c1 = new Complex(3, 2);
+        c2 = new Complex(3, -2);
     }
 
     public void testEquals() {
@@ -31,45 +17,23 @@ public class ComplexTest extends TestCase {
         assertTrue(c1.equals(c2) && c2.equals(c1));
     }
 
-    public void testR() {
-        assertEquals(20.0, c.r());
+    public void testAdd() {
+        Complex sum = new Complex(6);
+        assertTrue(sum.equals(c1.add(c2)));
     }
 
-    public void testI() {
-        assertEquals(4.0, c.i());
-    }
-
-    public void testToString() {
-        String   s = c.toString(),
-                s2 = c2.toString(),
-                s3 = c3.toString();
-        String joined = String.join("|", s, s2, s3);
-        String expected = "20 + 4i|-5i|-4 + 0.34i";
-
-        assertEquals(expected, joined);
+    public void testSub() {
+        Complex dif = new Complex(0, 4);
+        assertTrue(dif.equals(c1.sub(c2)));
     }
 
     public void testMult() {
-        Complex c1 = new Complex(2, 2),
-                c2 = new Complex(3, -4),
-                cm1 = c1.mult(c2),
-                cm2 = c2.mult(c1);
-        String expected = "14 - 2i|14 - 2i",
-                actual = String.join("|", cm1.toString(), cm2.toString());
-        assertEquals(expected, actual);
+        Complex prod = new Complex(13);
+        assertTrue(prod.equals(c1.mult(c2)));
     }
 
     public void testDiv() {
-        Complex c1 = new Complex(1, 0),
-                c2 = new Complex(5, 7),
-                quotient = c1.div(c2);
-        String expected = "0.06757 - 0.09459i",
-                actual = quotient.toString();
-        assertEquals(expected, actual);
-    }
-
-    public void testConj() {
-        Complex conj = c.conj();
-        assertEquals("20 - 4i", conj.toString());
+        Complex quot = new Complex(5.0/13.0, 12.0/13.0);
+        assertTrue(quot.equals(c1.div(c2)));
     }
 }
