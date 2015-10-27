b659ddd322313a90d8acdd4d6aaf92a5ef63a0ca
= <palmerjoshua2013@fau.edu>
2015-10-27 15:52:15 -0400

    hw4 first commit

---
+++ src/hw4/GraphRectangle.java
@@ -0,0 +1,25 @@
+package hw4;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+
+public class GraphRectangle implements GraphShape {
+    int x, y;
+    int width, height;
+    public GraphRectangle(int x, int y, int width, int height) {
+        this.x = x; this.y = y;
+        this.width = width; this.height = height;
+    }
+
+    public void setWidth(int width) {
+        if (width < 0 || width > 100)
+            throw new IllegalArgumentException("Width must be in [0, 100]");
+        this.width = width;
+    }
+
+    public void draw(Graphics2D g2) {
+        Rectangle2D.Double rectangle = new Rectangle2D.Double(x, y, width, height);
+        g2.draw(rectangle);
+    }
+}
---
+++ src/hw4/GraphShape.java
@@ -0,0 +1,10 @@
+package hw4;
+
+import java.awt.*;
+
+/**
+ * Created by josh on 10/27/2015.
+ */
+public interface GraphShape {
+    void draw(Graphics2D g2);
+}
