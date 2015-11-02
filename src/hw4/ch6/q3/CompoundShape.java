1e75f7b75fae743a6a85128c60b5a5371ecb0b22
palmerjoshua <palmerjoshua2013@fau.edu>
2015-11-02 17:49:59 -0500

    colored dashed border with solid square corners

--- src/hw4/ch6/q3/CompoundShape.java
+++ src/hw4/ch6/q3/CompoundShape.java
@@ -1,50 +1,73 @@
-package hw4.ch6.q3;
+    package hw4.ch6.q3;
 
-import java.awt.*;
-import java.awt.geom.*;
+    import java.awt.*;
+    import java.awt.geom.*;
 
-/**
-   A scene shape that is composed of multiple geometric shapes.
-*/
+    /**
+    A scene shape that is composed of multiple geometric shapes.
+    */
 public abstract class CompoundShape extends SelectableShape
 {
-   public CompoundShape()
-   {
+    public CompoundShape()
+    {
       path = new GeneralPath();
-   }
+    }
 
-   protected void add(Shape s)
-   {
+    protected void add(Shape s)
+    {
       path.append(s, false);
-   }
+    }
 
-   public boolean contains(Point2D aPoint)
-   {
+    public boolean contains(Point2D aPoint)
+    {
       return path.contains(aPoint);
-   }
+    }
 
-   public void translate(int dx, int dy)
-   {
-      path.transform(
-            AffineTransform.getTranslateInstance(dx, dy));
-   }
+    public void translate(int dx, int dy)
+    {
+      path.transform(AffineTransform.getTranslateInstance(dx, dy));
+    }
 
-   public void draw(Graphics2D g2)
-   {
+    public void draw(Graphics2D g2)
+    {
       g2.draw(path);
-   }
+    }
 
-   public void drawSelection(Graphics2D g2) {
-       draw(g2);
-       Rectangle2D bounds = path.getBounds();
-       double x = bounds.getX(), y = bounds.getY(), width = bounds.getMaxX()-x, height = bounds.getMaxY()-y;
-       Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{6}, 0);
-       Rectangle2D border = new Rectangle2D.Double(x, y, width, height);
-       g2.setStroke(dashed);
-       g2.draw(border);
-   }
+    private void drawSelectedBorder(Graphics2D g2) {
+        Rectangle2D bounds = path.getBounds();
+        double x = bounds.getX(), y = bounds.getY(), width = bounds.getMaxX()-x, height = bounds.getMaxY()-y;
+        Stroke dashed = new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{6}, 0);
+        Rectangle2D border = new Rectangle2D.Double(x, y, width, height);
 
-   private GeneralPath path;
+        g2.setStroke(dashed);
+        g2.setColor(Color.BLUE);
+        g2.draw(border);
+        g2.setStroke(new BasicStroke());
+        g2.setColor(Color.BLACK);
+    }
+
+    private void drawSelectedCorners(Graphics2D g2) {
+        Rectangle2D bounds = path.getBounds();
+        double x = bounds.getX(), y = bounds.getY(), width = bounds.getMaxX()-x, height = bounds.getMaxY()-y;
+        Rectangle2D.Double[] corners = {
+                new Rectangle2D.Double(x, y, 6, 6),
+                new Rectangle2D.Double((x+width)-6, y, 6, 6),
+                new Rectangle2D.Double(x, (y+height)-6, 6, 6),
+                new Rectangle2D.Double((x+width)-6, (y+height)-6, 6, 6)
+        };
+        g2.setPaint(Color.BLUE);
+        for (Rectangle2D.Double corner : corners) {
+            g2.fill(corner);
+        }
+        g2.setPaint(Color.BLACK);
+    }
+
+    public void drawSelection(Graphics2D g2) {
+        drawSelectedBorder(g2);
+        drawSelectedCorners(g2);
+    }
+
+    private GeneralPath path;
 }
 
 
