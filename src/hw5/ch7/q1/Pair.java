efc1a80a6fafe6557bf0ac55b07a04e90a4516ee
palmerjoshua <palmerjoshua2013@fau.edu>
2015-11-10 16:39:03 -0500

    implements clone, added main function

--- src/hw5/ch7/q1/Pair.java
+++ src/hw5/ch7/q1/Pair.java
@@ -1,6 +1,25 @@
 package hw5.ch7.q1;
 
-public class Pair<K, V> {
+import java.util.ArrayList;
+
+public class Pair<K, V> implements Cloneable {
+
+    public static void main(String[] args) {
+        ArrayList<Pair<Integer, Integer>> pairs = new ArrayList<>();
+        pairs.add(new Pair<>(5, 6));
+        pairs.add(new Pair<>(8, 9));
+        pairs.add(pairs.get(0).clone());
+
+        for (Pair<Integer, Integer> pair : pairs) {
+            System.out.println("Key: "+ pair.k() + " Value: " + pair.v());
+            System.out.println("To String: " + pair.toString());
+            System.out.println("Hash Code: " + pair.hashCode() + "\n");
+        }
+
+        System.out.println("First ==  Third: " + pairs.get(0).equals(pairs.get(2)));
+        System.out.println("First == Second: " + pairs.get(0).equals(pairs.get(1)));
+    }
+
     public Pair(K k, V v) {
         key = k;
         val = v;
@@ -10,7 +29,7 @@ public class Pair<K, V> {
 
     public boolean equals(Object o) {
         if (o == null) return false;
-        if (getClass() != o.getClass()) return false;
+        if (!getClass().equals(o.getClass())) return false;
         final Pair<K,V> other = (Pair<K,V>) o;
         return key.equals(other.k()) && val.equals(other.v());
     }
@@ -23,8 +42,9 @@ public class Pair<K, V> {
         return this.getClass().getName()+"["+key.toString()+", "+val.toString()+"]";
     }
 
-
-    // todo implement clone()
+    public Pair<K,V> clone() {
+        return new Pair<>(k(), v());
+    }
 
     private K key;
     private V val;
